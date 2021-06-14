861988dde8c6f994d62e39214035395b7b88b734
upillai <unnikrishnanrp@outlook.com>
2021-06-14 10:21:05 -0500

    Added express  default route

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,14 @@ import cors from 'cors';
 import restaurants from './api/restaurants.route.js';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/restaurants", restaurants);
+app.use('*', (req, res) => {
+    res.status(404).json({error: "not found"})
+});
+
+export default app;
+
