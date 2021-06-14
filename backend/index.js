9919721c43a8381850a123de9f469560e4e0e402
upillai <unnikrishnanrp@outlook.com>
2021-06-14 10:57:24 -0500

    added nodemon index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,25 @@
 import app from "./server.js";
-
 import mongodb from "mongodb";
-
 import dotenv from "dotenv";
 
 dotenv.config();
-const port = process.env.PORT || 8000;
-
-
 const MongoClient = mongodb.MongoClient;
-
+const port = process.env.PORT || 8000;
 MongoClient.connect(
     process.env.DB_URI,
     {
         poolSize: 50,
-        wtimeout: 2500, l,
-        useNewUrlParser: true
-
-    })
+        wtimeout: 2500,
+        useNewUrlParse: true
+    }
+)
     .catch(err => {
-        console.error(err);
-        process.exit(1);
-
-
-    })
+            console.error(err.stack);
+            process.exit(1);
+        }
+    )
     .then(async client => {
         app.listen(port, () => {
             console.log(`listening on port ${port}`);
         })
     })
-
