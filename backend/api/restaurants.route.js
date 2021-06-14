f830638293c247c23df4072ea8b92fb22d9bf4f6
upillai <unnikrishnanrp@outlook.com>
2021-06-14 14:40:51 -0500

    added backend

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -8,6 +8,8 @@ const router = express.Router();
     res.send("hello");
 })*/
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
 
 router.route("/review")
     .post(ReviewsCtrl.apiPostReview)
