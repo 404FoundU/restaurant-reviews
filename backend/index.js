75c54d657f530511746cdeef69a590dcf86a7770
upillai <unnikrishnanrp@outlook.com>
2021-06-14 10:33:22 -0500

    added mongo connect

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,28 @@ import mongodb from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
+const port = process.env.PORT || 8000;
+
+
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(
+    process.env.DB_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500, l,
+        useNewUrlParser: true
+
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+
+
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`);
+        })
+    })
+
