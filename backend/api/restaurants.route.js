680a0e67d598043dd3abaf217b89af9efb6af567
upillai <unnikrishnanrp@outlook.com>
2021-06-14 11:40:56 -0500

    added restaurants.controller.js

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,11 @@
 import express from "express";
+import RestaurantsCtrl from "./restaurants.controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+/*router.get("/", (req, res) => {
     res.send("hello");
-})
+})*/
+router.get("/", RestaurantsCtrl.apiGetRestaurants)
 
 export default router;
